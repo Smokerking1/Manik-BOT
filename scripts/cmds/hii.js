5a535048ea3a61e9f2b9d8a9809ecfc551adf209
Smokerking1 <mirajlife55.mh@gmail.com>
2024-06-29 09:26:10 +0600

    Update hii.js

--- scripts/cmds/hii.js
+++ scripts/cmds/hii.js
@@ -2,7 +2,7 @@ module.exports = {
  config: {
    name: "hii",
    version: "1.0",
-   author: "MR.AYAN",
+   author: "MD.MANIK KHAN",
    countDown: 5,
    role: 0,
    shortDescription: "no prefix",
@@ -14,7 +14,7 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "hii") {
  return message.reply({
- body: "🥰🥀",
+ body: "🙂🥀💔",
  attachment: await global.utils.getStreamFromURL("https://i.postimg.cc/brsP4XWb/b57d340eec9cf7395653c38088491ace3ab7673b-hq.gif")
  });
  }
