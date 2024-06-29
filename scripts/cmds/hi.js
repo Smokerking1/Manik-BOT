2f500bfb47acb2f618c7146180ba3e9751298c54
Smokerking1 <mirajlife55.mh@gmail.com>
2024-06-29 09:24:12 +0600

    Update hi.js

--- scripts/cmds/hi.js
+++ scripts/cmds/hi.js
@@ -1,9 +1,9 @@
 module.exports = {
     config: {
         name: "hi",
-        aliases: ["hii", "hyy", "hiii", "bro", "hlw"],
+        aliases: ["hi", "hello", "হাই", "bro", "hlw"],
         version: "1.0",
-        author: "MR.AYAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
+        author: "MD.MANIK KHAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
         countDown: 5,
         role: 0,
         shortDescription: "No Prefix",
