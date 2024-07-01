717c2bf538e199e3e532c958d9d948dfa3f5af6a
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-02 05:22:00 +0600

    Update 7.js

--- scripts/cmds/7.js
+++ scripts/cmds/7.js
@@ -2,7 +2,7 @@ module.exports = {
     config: {
         name: "😂",
         version: "1.0",
-        author: "MR.AYAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
+        author: "MD.MANIK KHAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
         countDown: 5,
         role: 0,
         shortDescription: "No Prefix",
@@ -15,6 +15,6 @@ onChat: async function({
     message,
     getLang
 }) {
-    if (event.body && event.body.toLowerCase() == "😂") return message.reply("kicce ato haso kn-!!😒");
+    if (event.body && event.body.toLowerCase() == "😂") return message.reply("ওলে লে কি সুন্দর হাসিরে-!!😒");
 }
 }; 
