e227b538a15e79bb9967a5cc03db7d1013360c56
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-02 05:16:30 +0600

    Update 5.js

--- scripts/cmds/5.js
+++ scripts/cmds/5.js
@@ -2,7 +2,7 @@ module.exports = {
     config: {
         name: "🐸",
         version: "1.0",
-        author: "MR.AYAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
+        author: "MD.MANIK KHAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
         countDown: 5,
         role: 0,
         shortDescription: "No Prefix",
