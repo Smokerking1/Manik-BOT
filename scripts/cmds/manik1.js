81272f849c6e6f1e4b6a7543f35b6177314b87ac
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-03 02:34:56 +0600

    Create manik1.js

---
+++ scripts/cmds/manik1.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "🤦‍♂️",
+        version: "1.0",
+        author: "MD.MANIK KHAN",
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "🤦‍♂️") return message.reply("তোমার কপালে ঠাডা পরুক🙂🤧");
+}
+}; 
