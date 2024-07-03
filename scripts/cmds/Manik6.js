bde4f512eb0f4e825359b8a8d809468668618ed0
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-03 12:38:13 +0600

    Create Manik6.js

---
+++ scripts/cmds/Manik6.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "😠",
+        version: "1.0",
+        author: "MD.MANIK KHAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "😠","😠😠","😠😠😠","🤬🤬","🤬🤬🤬","😡😡","😡😡😡","😤","😤😤","😤😤😤") return message.reply("কিরে রাগ দেখাস কারে(😏)তোর রাগের উপর আমি মুতি🤪");
+}
+};
