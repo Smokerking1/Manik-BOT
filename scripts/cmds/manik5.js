2211bf265b8c8a0b2b5ea73d60f27cb5bd801e6a
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-03 12:25:49 +0600

    Create manik5.js

---
+++ scripts/cmds/manik5.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "🤬",
+        version: "1.0",
+        author: "MD.MANIK KHAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "🤬") return message.reply("রাগ দেখাস কারে🤨তোর রাগের উপর আমি মুতি🤪");
+}
+}; 
