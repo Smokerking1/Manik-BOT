a986ff536a66607edfc7b0c2cf4b6e4f4712255c
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-04 12:22:42 +0600

    Update Emoji1.js

--- scripts/cmds/Emoji1.js
+++ scripts/cmds/Emoji1.js
@@ -16,7 +16,7 @@ module.exports = {
     onStart: async function() {}, 
     onChat: async function({ event, message, getLang }) {
         if (event.body && event.body.toLowerCase() == "🙃") {
-            const videoUrl = "https://imgur.com/fEmf5Up"; // Replace with your video URL
+            const videoUrl = "https://www.kapwing.com/videos/66863ecb541ff366243a23e1"; // Replace with your video URL
             const videoPath = path.resolve(__dirname, 'cliff.mp4');
 
             try {
