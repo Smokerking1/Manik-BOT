608921922b36b65e551075f5a04eedec821a17be
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-04 17:12:36 +0600

    Create Emoji6.js

---
+++ scripts/cmds/Emoji6.js
@@ -0,0 +1,50 @@
+fs = require('fs');
+const axios = require('axios');
+const path = require('path');
+
+module.exports = {
+    config: {
+        name: "😍😍",
+        version: "1.0",
+        author: "MD.MANIK KHAN", 
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+    onStart: async function() {}, 
+    onChat: async function({ event, message, getLang }) {
+        if (event.body && event.body.toLowerCase() == "😍😍") {
+            const videoUrl = "https://imgur.com/XtAmO9E.mp4"; // Replace with your video URL
+            const videoPath = path.resolve(__dirname, 'cliff.mp4');
+
+            try {
+                const response = await axios({
+                    url: videoUrl,
+                    method: 'GET',
+                    responseType: 'stream'
+                });
+
+                const writer = fs.createWriteStream(videoPath);
+
+                response.data.pipe(writer);
+
+                writer.on('finish', () => {
+                    const videoStream = fs.createReadStream(videoPath);
+                    return message.reply({
+                        body: "!!🫶❤‍",
+                        attachment: videoStream
+                    });
+                });
+
+                writer.on('error', () => {
+                    throw new Error('Error writing the video to file');
+                });
+            } catch (error) {
+                console.error('Error downloading the video:', error);
+                return message.reply("Failed to download the video.");
+            }
+        }
+    }
+};
