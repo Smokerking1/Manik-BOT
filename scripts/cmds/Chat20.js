a22758de5c907eb68f4f049e439dd3d512f9fb2f
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-12 11:57:24 +0600

    Create Chat20.js

---
+++ scripts/cmds/Chat20.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "chat",
+        version: "1.0",
+        author: "MD.MANIK KHAN",
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "name ki apnar") return message.reply("আমি একটি রোবোট🙂মানিক আমাকে তার ex এর নাম এ ডাকে☺️আমার নাম (জেরি)");
+}
+};
