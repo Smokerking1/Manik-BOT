9c525021c06742fbd724e828ad41381d12bb77ce
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-12 11:43:05 +0600

    Create Chat13.js

---
+++ scripts/cmds/Chat13.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "chat",
+        version: "1.0",
+        author: "MD.MANIK KHAN",
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "gdn8") return message.reply("জান আমারে রেখে জাইওনা🥹");
+}
+};
