9e87525866e7fb2ae354023f8a753ab8a05bdddd
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-12 11:47:07 +0600

    Create Chat15.js

---
+++ scripts/cmds/Chat15.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "chat",
+        version: "1.0",
+        author: "MD.MANIK KHAN",
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "good night") return message.reply("জান আমারে একলা রেখে জাবা☺️");
+}
+};
