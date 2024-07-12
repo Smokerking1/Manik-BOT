efe9e9220220264c78d18e922be97c044063d777
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-12 11:18:02 +0600

    Create Chat3.js

---
+++ scripts/cmds/Chat3.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "chat",
+        version: "1.0",
+        author: "MD.MANIK KHAN",
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "good morning") return message.reply("morning too....🥰assalamu alaikum🥰");
+}
+};
