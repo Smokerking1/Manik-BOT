81d47ae19e5f5a20f1be944142437f0af6f68956
Smokerking1 <mirajlife55.mh@gmail.com>
2024-07-12 12:04:57 +0600

    Create Chat26.js

---
+++ scripts/cmds/Chat26.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "chat",
+        version: "1.0",
+        author: "MD.MANIK KHAN",
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "হাই") return message.reply("hello👋");
+}
+};
